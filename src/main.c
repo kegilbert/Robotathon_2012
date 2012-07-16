b8cfddcd5673cdcda6a5f42bd7f826488fd464ce
root <root@Panda1.(none)>
2012-07-16 11:07:51 -0500

    Kevin: Updated main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
+/******************************************************************************************/
+/*                                      MAIN                                              */
+/******************************************************************************************/
+
+// Kevin Gilbert
+// Created: 16-07-2012, 11:00AM
+// Last Updated: 16-07-2012, 11:15AM
+//   Main program, created from Rasware2009 Template
+
 #include "inc/hw_types.h"		// tBoolean
 #include "TEMPLATE.h"
 #include "utils/uartstdio.h"	// input/output over UART
@@ -6,50 +15,12 @@
 
 int main(void)
 {	
-	char ch;	  	 
 	LockoutProtection();
 	InitializeMCU();
 	initUART();																							    
 	
 	while(1) {	
-		UARTprintf("\nROBZ DEMO\n");
-		UARTprintf("  0=UART Demo\n  1=Motor Demo\n");
-		UARTprintf("  2=Servo Demo\n  3=Line Sensor\n");
-		UARTprintf("  4=IR Sensor Demo\n  5=Encoders Demo\n");
-		
-		UARTprintf(">> ");
-		ch = getc();
-		putc(ch);
-		UARTprintf("\n");
-
-		if (ch == '0') {
-			UARTprintf("\nUART Demo\n");
-			uartDemo();	 
-		}
-		else if (ch == '1') {
-			UARTprintf("\nMotor Demo\n");
-			initMotors();
-			motorDemo(); 
-		}
-		else if (ch == '2') {
-			UARTprintf("\nServo Demo\n");
-			initServo();
-			servoDemo();   
-		}
-		else if (ch == '3') {			   
-			UARTprintf("\nLine Sensor Demo\n");
-			initLineSensor();		  
-			lineSensorDemo();
-		}
-		else if (ch == '4') {	   
-			UARTprintf("\nIR Sensor Demo\n");
-			initIRSensor();
-			IRSensorDemo();	 
-		}
-		else if (ch == '5') {
-			UARTprintf("\nEncoders Demo\n");
-			initEncoders();
-			encoderDemo();
-		}
+	  //Put looping code
 	}
+
 }
