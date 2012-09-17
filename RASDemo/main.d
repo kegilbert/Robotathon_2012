f46eb3109072504e0404ac1c55192f5823ed8a90
robz from virtual machine <geomasta21@aol.com>
2012-09-16 23:39:33 -0500

    fixing project for .gitignore

--- RASDemo/main.d
+++
@@ -1,7 +0,0 @@
-main.o: Main.c
-main.o: ..\StellarisWare\inc/hw_types.h
-main.o: RASDemo.h
-main.o: ..\StellarisWare\utils/uartstdio.h
-main.o: ..\StellarisWare\driverlib/uart.h
-main.o: ..\StellarisWare\driverlib/sysctl.h
-main.o: ..\RASLib\include\RASLib/init.h
